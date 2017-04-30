eaf14d7f829b25d6bb545ad855a9790562f5a2a3
Miguel Moreno Gómez <MNXANL@users.noreply.github.com>
2017-04-30 19:09:52 +0200

    Makefile for compiling new plugins

---
+++ RunPlugins.sh
@@ -0,0 +1,4 @@
+# This file generates two programs:
+# -> Gviewer:   viewer w/o plugins. Use it to try new plugins
+# -> GviewerSL: viewer with shaderloader plugin. Use it to develop shaders.
+qmake-qt5; make -j; ./Gviewer
